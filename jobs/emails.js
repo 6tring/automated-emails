12833b6cafa7133cfe6ab6169dc77eabb4950e88
Peter Ciluzzi <46660440+6tring@users.noreply.github.com>
2024-01-27 17:37:52 -0500

    Update emails.js

--- jobs/emails.js
+++ jobs/emails.js
@@ -11,10 +11,9 @@ module.exports = function(agenda) {
 	agenda.define('sendEmails', async function(job) {
 		try {
 			let today = moment(new Date).format("MM/DD/YYYY");
-      let userJobs = await UserJob.find().where("jobDate").equals(today);
-
+      			let userJobs = await UserJob.find().where("jobDate").equals(today);
 			for(let userJob of userJobs) {
-        // Specify job logic here
+       				// Specify job logic here
 			}
 		} catch(err) {
 			console.log("Error encountered during sendEmails: ", err.message);
